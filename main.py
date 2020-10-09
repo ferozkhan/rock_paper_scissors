e62361f2dfd4e8d30aa7d74396b460038d7110eb
Feroz khan <ferozkhan27@gmail.com>
2020-10-10 00:39:02 +0200

    refactor

--- main.py
+++ main.py
@@ -3,44 +3,50 @@ import random
 OPTIONS = ['rock', 'paper', 'scissors']
 
 
-def get_computer_choice():
-    return random.choice(OPTIONS)
+class RockPaperScissorsSimulator:
+
+    def __init__(self):
+        self.human_choice = None
+        self.computer = None
+
+    def get_computer_choice(self):
+        self.computer_choice = random.choice(OPTIONS)
+
+    def get_human_choice(self):
+        choice_number = int(input("Enter the number of your choice: "))
+        self.human_choice = OPTIONS[choice_number - 1]
+
+    @staticmethod
+    def print_options():
+        print("\n".join(f"({i}) {option.title()}" for i, option in enumerate(OPTIONS)))
+
+    def print_choices(self):
+        print(f"You chose: {self.human_choice}")
+        print(f"Computer chose: {self.computer_choice}")
+
+    def print_win_loose(self, human_beats, human_loses_to):
+        if self.computer_choice == human_beats:
+            print(f"Sorry, {self.computer_choice} beats {self.human_choice}")
+        elif self.computer_choice == human_loses_to:
+            print(f"Yes, {self.human_choice} beats {self.computer_choice}")
+
+    def print_result(self):
+        if self.human_choice == self.computer_choice:
+            print("Draw!")
+        if self.human_choice == "rock":
+            self.print_win_loose("scissors", "paper")
+        elif human_choice == "scissors":
+            self.print_win_loose("paper", "rock")
+        elif human_choice == "paper":
+            self.print_win_loose("rock", "scissors")
+
+    def simulate(self):
+        self.print_options()
+        self.get_human_choice()
+        self.get_computer_choice()
+        self.print_choices()
+        self.print_result()
 
 
-def get_human_choice():
-    choice_number = int(input("Enter the number of your choice: "))
-    return OPTIONS[choice_number - 1]
-
-
-def print_options():
-    print("\n".join(f"({i}) {option.title()}" for i, option in enumerate(OPTIONS)))
-
-
-def print_choices(human_choice, computer_choice):
-    print(f"You chose: {human_choice}")
-    print(f"Computer chose: {computer_choice}")
-
-
-def print_win_loose(human_choice, computer_choice, human_beats, human_loses_to):
-    if computer_choice == human_beats:
-        print(f"Sorry, {computer_choice} beats {human_choice}")
-    elif computer_choice == human_loses_to:
-        print(f"Yes, {human_choice} beats {computer_choice}")
-
-
-def print_result(human_choice, computer_choice):
-    if human_choice == computer_choice:
-        print("Draw!")
-    if human_choice == "rock":
-        print_win_loose("rock", computer_choice, "scissors", "paper")
-    elif human_choice == "scissors":
-        print_win_loose("scissors", computer_choice, "paper", "rock")
-    elif human_choice == "paper":
-        print_win_loose("paper", computer_choice, "rock", "scissors")
-
-
-print_options()
-human_choice = get_human_choice()
-computer_choice = get_computer_choice()
-print_choices(human_choice, computer_choice)
-print_result(human_choice, computer_choice)
+simulator = RockPaperScissorsSimulator()
+simulator.simulate()
