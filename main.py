e0aac9c4b17b0d71fe4d5ca0c5cb18a76d830ad4
Feroz khan <ferozkhan27@gmail.com>
2020-10-10 00:14:15 +0200

    refactor

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 import random
 
-options = ['rock', 'paper', 'scissors']
-print('(1) Rock\n(2) Paper\n(3) Scissors')
-human_choice = options[int(input('Enter the number of your choice: ')) - 1]
-print(f'You chose {human_choice}')
-computer_choice = random.choice(options)
-print(f'The computer chose {computer_choice}')
-if human_choice == 'rock':
-    if computer_choice == 'paper':
-        print('Sorry, paper beat rock')
-    elif computer_choice == 'scissors':
-        print('Yes, rock beat scissors!')
-    else:
-        print('Draw!')
-elif human_choice == 'paper':
-    if computer_choice == 'scissors':
-        print('Sorry, scissors beat paper')
-    elif computer_choice == 'rock':
-        print('Yes, paper beat rock!')
-    else:
-        print('Draw!')
-elif human_choice == 'scissors':
-    if computer_choice == 'rock':
-        print('Sorry, rock beat scissors')
-    elif computer_choice == 'paper':
-        print('Yes, scissors beat paper!')
-    else:
-        print('Draw!')
+OPTIONS = ['rock', 'paper', 'scissors']
+
+
+def get_computer_choice():
+    return random.choice(OPTIONS)
+
+
+def get_human_choice():
+    choice_number = int(input("Enter the number of your choice: "))
+    return OPTIONS[choice_number - 1]
+
+
+def print_options():
+    print("\n".join(f"({i}) {option.title()}" for i, option in enumerate(OPTIONS)))
+
+
+def print_choices(human_choice, computer_choice):
+    print(f"You chose: {human_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+
+def print_win_loose(human_choice, computer_choice, human_beats, human_loses_to):
+    if computer_choice == human_beats:
+        print(f"Sorry, {computer_choice} beats {human_choice}")
+    elif computer_choice == human_loses_to:
+        print(f"Yes, {human_choice} beats {computer_choice}")
+
+
+def print_result(human_choice, computer_choice):
+    if human_choice == computer_choice:
+        print("Draw!")
+    if human_choice == "rock":
+        print_win_loose("rock", computer_choice, "scissors", "paper")
+    elif human_choice == "scissors":
+        print_win_loose("scissors", computer_choice, "paper", "rock")
+    elif human_choice == "paper":
+        print_win_loose("paper", computer_choice, "rock", "scissors")
+
+
+print_options()
+human_choice = get_human_choice()
+computer_choice = get_computer_choice()
+print_choices(human_choice, computer_choice)
+print_result(human_choice, computer_choice)
